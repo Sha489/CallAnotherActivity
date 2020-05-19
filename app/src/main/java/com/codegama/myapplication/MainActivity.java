d703f5f860ef67713f1e121c538713517f279e60
sharon rose <sharon489@codegama.com>
2020-05-20 00:00:42 +0530

    Library

--- app/src/main/java/com/codegama/myapplication/MainActivity.java
+++ app/src/main/java/com/codegama/myapplication/MainActivity.java
@@ -1,14 +1,54 @@
 package com.codegama.myapplication;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.codegama.commutionlibrary.CallAnotherAppActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addition;
+    Button subtraction;
+    int sum, firstNumb, secondNumb;
+    TextView total, inputOne, inputTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getIntent().getExtras() != null) {
+            sum = getIntent().getExtras().getInt("SUM");
+            firstNumb = getIntent().getExtras().getInt("INPUT_ONE");
+            secondNumb = getIntent().getExtras().getInt("INPUT_TWO");
+        }
+
+        addition = findViewById(R.id.addition);
+        subtraction = findViewById(R.id.subtraction);
+        total = findViewById(R.id.total);
+        inputOne = findViewById(R.id.inputoneText);
+        inputTwo = findViewById(R.id.inputTwoText);
+
+        inputOne.setText(String.valueOf(firstNumb));
+        inputTwo.setText(String.valueOf(secondNumb));
+        total.setText(String.valueOf(sum));
+
+        addition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CallAnotherAppActivity.callApplication(Integer.parseInt(inputOne.getText().toString()), Integer.parseInt(inputTwo.getText().toString()), 1);
+            }
+        });
+
+        subtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callAnotherApplication(Integer.parseInt(inputOne.getText().toString()), Integer.parseInt(inputTwo.getText().toString()), 2);
+            }
+        });
     }
 }
--- commutionlibrary/src/main/java/com/codegama/commutionlibrary/CallAnotherAppActivity.java
+++ commutionlibrary/src/main/java/com/codegama/commutionlibrary/CallAnotherAppActivity.java
@@ -1,5 +1,17 @@
 package com.codegama.commutionlibrary;
 
+import android.content.Context;
+import android.content.Intent;
+
 public class CallAnotherAppActivity {
 
+    public static void callApplication(Context context, int inputOne, int inputTwo, int type) {
+        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage("com.codegama.newapplication");
+        launchIntent.putExtra("INPUT_ONE", inputOne);
+        launchIntent.putExtra("INPUT_TWO", inputTwo);
+        launchIntent.putExtra("TYPE", type);
+        if (launchIntent != null) {
+            context.startActivity(launchIntent);
+        }
+    }
 }
